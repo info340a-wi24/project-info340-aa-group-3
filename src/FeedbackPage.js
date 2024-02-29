e26f5e4a25bf46b77fca32103592665db8d306f4
Linda Giang <97063743+lattematics@users.noreply.github.com>
2024-02-29 11:27:16 -0800

    Revert "moved most the html to js"

--- src/FeedbackPage.js
+++ src/FeedbackPage.js
@@ -2,23 +2,6 @@ import React from 'react';
 
 export default function FeedbackPage() {
   return (
-    <div>
-      <div id="feedback">
-        <h2>Submit Feedback</h2>
-        <div>
-          <label for="name-input">Name: </label>
-          <input name="name" placeholder="" />
-        </div>
-        <div>
-          <label for="email-input">Email: </label>
-          <input name="email" placeholder="" />
-        </div>
-        <div>
-          <label for="feedback">Feedback: </label>
-          <textarea id="feedback-input" name="feedback" />    
-        </div>
-        <button><a id="submit" class="btn btn-primary" role="button" href="confirm.html" title="Home"> Submit </a></button>
-      </div>
-    </div>
+
   );
 }
