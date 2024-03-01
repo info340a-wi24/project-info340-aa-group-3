b124bf97e9c63cb8ac532e022e3e99a5f33d2d2d
Linda <linda71@uw.edu>
2024-02-29 22:40:08 -0800

    edit RSVP, Feedback still the same

--- src/components/FeedbackPage.js
+++ src/components/FeedbackPage.js
@@ -2,68 +2,93 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function FeedbackPage() {
-    let nameInput = document.querySelector('#nameInput');
-    let emailInput = document.querySelector('#emailInput');
-    if(nameInput && emailInput){
+    let form = document.querySelector('form');
+    if(form){
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            if(form.checkValidity()){
+                form.classList.add('was-validated');
+                console.log("Checked Validity");
+            } else{
+                document.getElementById('submit').disabled = true;
+            }
+            }); 
+    }
 
+    let nameInput = document.getElementById('nameInput');
+    let emailInput = document.getElementById('emailInput');
+    if(nameInput && emailInput){
         nameInput.addEventListener('input', () => {
             if(nameInput.value = ""){
                 nameInput.setCustomValidity("Please enter your name.");
-                document.querySelector('#nameFeedback').textContent = "Please enter your name.";
+                document.getElementById('nameFeedback').textContent = "Please enter your name.";
             } else{
                 nameInput.setCustomValidity("");
-                document.querySelector('#nameFeedback').textContent = "";
+                document.getElementById('nameFeedback').textContent = "";
             }
             }); 
 
         emailInput.addEventListener('input', () => {
             if(!emailInput.value.includes('@')){
                 emailInput.setCustomValidity("Please enter your email.");
-                document.querySelector('#emailFeedback').textContent = "Please enter your name.";
+                document.getElementById('emailFeedback').textContent = "Please enter your name.";
             } else{
                 emailInput.setCustomValidity("");
-                document.querySelector('#emailFeedback').textContent = "";
+                document.getElementById('emailFeedback').textContent = "";
             }
             }); 
     }
 
-    let textInput = document.querySelector('#textInput');
+    let textInput = document.getElementById('textInput');
     if(textInput){
         textInput.addEventListener('input', () => {
             if(textInput = ""){
                 textInput.setCustomValidity("Please provide your feedback.");
-                document.querySelector('#textFeedback').textContent = "Please provide your feedback.";
+                document.getElementById('textFeedback').textContent = "Please provide your feedback.";
             } else{
                 textInput.setCustomValidity("");
-                document.querySelector('#textFeedback').textContent = "";
+                document.getElementById('textFeedback').textContent = "";
             }
             }); 
     }
 
+    let allInput = document.querySelectorAll('input');
+    allInput.forEach((input) => {
+        input.addEventListener('input', () => {
+            if(form.classList.contains('was-validated')){
+                document.getElementById('submit').disabled = true;
+            } else{
+                document.getElementById('submit').disabled = false;
+            }
+        });
+    });
 
   return (
         <section>
             <form id="feedbackForm" class="form">
-                <Link to="/home" type="exit" class="btn btn-danger mb-3">Cancel</Link>
+                <div id="top">
+                    <div> </div>
+                    <Link to="/home" type="exit" class="btn btn-danger mb-3">Cancel</Link>
+                </div>
                 <h2>Help Protests Hub improve with your input!</h2>
                 <div class="input-group row mb-4">
                 <label for="nameInput" class="col-lg-1">Name</label>
                     <div class="col-lg-11">
-                        <input type="name" id="nameInput" class="form-control" required/>
+                        <input type="name" id="nameInput" class="form-control" required title="Please enter your name."/>
                         <div id="nameFeedback" class="invalid-feedback">Please provide a name.</div>
                     </div>
                 </div>
                 <div class="input-group row mb-3">
                 <label for="emailInput" class="col-lg-1">Email</label>
                     <div class="col-lg-11">
-                        <input type="email" id="emailInput" class="form-control" required/>
+                        <input type="email" id="emailInput" class="form-control" required title="Please enter your email."/>
                         <div id="emailFeedback" class="invalid-feedback">Please provide a valid email.</div>
                     </div>
                 </div>
                 <div class="input-group row mb-3">
                 <label for="feedbackInput" class="col-lg-1">Feedback</label>
                     <div class="col-lg-11">
-                        <input type="textFeedback" id="textInput" class="form-control" required/>
+                        <input type="textFeedback" id="textInput" class="form-control" required title="Please enter your feedback."/>
                         <div id="textFeedback" class="invalid-feedback">Please enter your feedback.</div>
                     </div>
                 </div>
