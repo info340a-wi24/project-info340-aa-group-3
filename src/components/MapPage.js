2413c3110c8c4015efbc32fb52f3d23a16e1bf6a
mtran3 <102627861+mtran3@users.noreply.github.com>
2024-03-01 16:55:45 -0800

    Added Google Maps API and changed env.local file variable

--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -1,7 +1,33 @@
-import React from 'react';
+"use client"; 
 
-export default function MapPage(props) {
+import React from 'react';
+import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+// make sure to npm install this in the project root directory
+// npm install @vis.gl/react-google-maps
+
+// export default function MapPage(props) {
+
+  export default function MapPage() {
+    const position = {lat: 47.79172321733598, lng: -122.36609425962166}; 
+
     return (
-        <img src="images/pcc-map.png" alt="Google Map view of Edmonds, WA."/>
+        <>
+        <h1> Map </h1>
+          <div style={{ height: "100vh", width: "100%" }}>
+            <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}>
+              <Map defaultCenter={position} defaultZoom={10}>
+                <Marker position={position} />
+            </Map>
+          </APIProvider>
+          </div>
+      </>
     )
-}
+
+        // return (
+    //     <>
+    //     <p>testing</p>
+    //     <img src="images/pcc-map.png" alt="Google Map view of Edmonds, WA."/>
+    //     </>
+    // )
+
+  }
