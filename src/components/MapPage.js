40ae4969927d4acee3cc0d27467681f4c898e454
mtran3 <102627861+mtran3@users.noreply.github.com>
2024-03-12 23:18:01 -0700

    Search result aligns with a marker

--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -4,10 +4,35 @@ import React from 'react';
 import { useState } from 'react'; 
 import ProtestData from '../data/protestdata.json'; 
 import { APIProvider, Map, AdvancedMarker, InfoWindow } from '@vis.gl/react-google-maps';
-import MapSearchBar from './MapSearchBar';
 
 // make sure to npm install this in the project root directory: 
 // npm install @vis.gl/react-google-maps
+
+// Search bar
+
+function MapSearchBar({ onSearchedProtest }) {
+    
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleSearch(event) {
+    const value = event.target.value.toLowerCase(); 
+      setSearchTerm(value); 
+      onSearchedProtest(value)
+  };
+
+  // use the searchterm's coordinate to create a marker
+
+  return (
+      <div>
+          <input 
+          type="text" 
+          placeholder="Search for protest locations..." 
+          className="search justify-content-center"
+          value={searchTerm}
+          onChange={handleSearch} />
+      </div>
+  ); 
+}
 
   export default function MapPage(props) {
 
@@ -16,6 +41,16 @@ import MapSearchBar from './MapSearchBar';
 
     // opens an info window if clicked 
     const [open, setOpen] = useState(null); 
+
+    // stores the search results
+    const [searchResults, setSearchResults] = useState([]);  
+
+    function handleSearch(searchTerm) {
+     const filteredData = ProtestData.filter(protest => (
+      protest.title.toLowerCase().includes(searchTerm.toLowerCase()))); 
+        setSearchResults(filteredData); 
+        setOpen(null); 
+    };
 
     // handles clicking on the marker
     function markerHandler(protestTitle) {
@@ -27,36 +62,24 @@ import MapSearchBar from './MapSearchBar';
       setOpen(null); 
     }
 
-
-    // somehow connect the search bar with the location/protest name 
-    // the location/protest name will connect with the coordinate
-    // the specific coordinate will allow them to see the specific info window
-    // or data on the side if possible
-
-    // 1) user types in the search result - this will be connected to the db 
-    // 2) the event.target.value of the handle search will be the search term
-    // 3) this value will now be the one that we use to show the marker/info
-    // 4) pass this to the createMarker
-    // 5) Set open is seperate - may need to do this logic outside/in it's own 
-    // function 
-
     return (
         <>
         <h1> Protest Map </h1>
+        <h2> Search for a protest and view its location</h2>
           <div style={{ height: "100vh", width: "90%" }}>
             <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}>
-              <MapSearchBar />
+              <MapSearchBar onSearchedProtest={handleSearch} />
               <Map 
               defaultCenter={position} 
               defaultZoom={5}
               mapId={process.env.REACT_APP_PUBLIC_MAP_ID}>
-                {ProtestData.map((protest) => (
+                {searchResults.map((protest) => ( 
                 <AdvancedMarker 
                 key={protest.title}
                 position={{lat: protest.latitude, lng: protest.longitude}} 
                 onClick={() => markerHandler(protest.title)}/>
                 ))}
-                {open && (
+                {open !== null && (
                   <InfoWindow 
                   position={{lat: ProtestData.find(protest => protest.title === open).latitude
                   , lng: ProtestData.find(protest => protest.title === open).longitude}} 
@@ -64,7 +87,7 @@ import MapSearchBar from './MapSearchBar';
                   <div> 
                     <h3>{open}</h3>
                     <hr />
-                    <h5>{ProtestData.find(protest => protest.title === open).organizer}</h5>
+                    <h4>{ProtestData.find(protest => protest.title === open).organizer}</h4>
                     <p>Category: {ProtestData.find(protest => protest.title === open).category}</p>
                     <p>{ProtestData.find(protest => protest.title === open).date}, {ProtestData.find(protest => protest.title === open).time}</p>
                     </div> 
